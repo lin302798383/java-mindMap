1e31cb4ef63ac72857f228c724a7ba0eb90bdfd0
km.lin <302798383@qq.com>
2025-08-26 10:37:05 +0800

    commit

--- src/al/_215.java
+++ src/al/_215.java
@@ -1,2 +1,48 @@
-package al;public class _215 {
+package al;
+
+public class _215 {
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(nums[findKthLargest(nums, 2)]);
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        int idx;
+        int left = 0;
+        int right = nums.length - 1;
+        int targetIdx = right - k + 1;
+        while ((idx = quickSortImpl(nums, left, right)) != targetIdx) {
+            if (idx > targetIdx) {
+                right = idx - 1;
+            } else {
+                left = idx + 1;
+            }
+        }
+        return nums[idx];
+    }
+
+    /**
+     * 快排，返回该次排序后基准元素的位置
+     */
+    public static int quickSortImpl(int[] nums, int left, int right) {
+        int baseNum = nums[left];
+        int baseIdx=left;
+        while (left < right) {
+            while (left < right && nums[right] >= baseNum) {
+                right--;
+            }
+            while (left < right && nums[left] <= baseNum) {
+                left++;
+            }
+            swap(nums, left, right);
+        }
+        swap(nums, baseIdx, left);
+        return left;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
--- src/al/_3.java
+++ src/al/_3.java
@@ -1,5 +1,31 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
+package al;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class _3 {
+    /**
+     * 滑动窗口
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        int result = 0;
+        int len = s.length();
+        int point = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        while (point < len) {
+            char c = s.charAt(point);
+            if (!map.containsKey(c)) {
+                map.put(c, point);
+                point++;
+                continue;
+            }
+            result = Math.max(map.size(), result);
+            point = map.get(c) + 1;
+            map.clear();
+
+        }
+        result = Math.max(map.size(), result);
+        return result;
     }
+
 }
